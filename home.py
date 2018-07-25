58931cec24329bb7cfb8fc9a3c3c2b706ca03836
Christopher Allen-Poole <chris.allen-poole@omnigon.com>
2018-07-25 14:33:05 -0400

    Only need to show results

--- home.py
+++ home.py
@@ -1,16 +1,26 @@
 import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify, make_response
 )
 
 from db import get_db
 
 bp = Blueprint('poll', __name__, url_prefix='/poll')
 
+
+@bp.route('/set_id', methods=('GET',))
+def index():
+    resp = make_response('')
+    resp.set_cookie('user_id', request.args['user_id'])
+    return resp
+
+def get_user_id():
+    return request.cookies.get('user_id')
+
 @bp.route('/show', methods=['GET'])
 def show_poll():
-    return render_template('poll/display.html', user_id=1234)
+    return render_template('poll/display.html', user_id=get_user_id())
 
 @bp.route('/current')
 def show_current_poll():
@@ -19,7 +29,9 @@ def show_current_poll():
     if result:
         return jsonify({
             'id': result['id'],
+            'poll_time': result['poll_time'],
             'title': result['title'],
+            'expired': result['expired'],
             'options': result['options'].split(',')
         })
 
@@ -30,15 +42,69 @@ def answer_poll():
     db = get_db()
     poll_id = request.form['poll_id']
     user_id = request.form['user_id']
-    option = request.form['option']
+    choice = request.form['option']
+    
+    choice = choice or 0
+
     poll = db.execute('SELECT * FROM poll WHERE id=?', (poll_id,)).fetchone()
    
     if not poll:
         from flask import abort
         return abort(404)
+
+    db.execute(
+            'INSERT INTO bet (user_id, poll_id, choice) VALUES (?,?,?)',  
+            (user_id, poll_id, choice))
+
+    db.commit()
+
     return redirect(url_for('poll.show_result'))
 
 @bp.route('/result', methods=['GET'])
 def show_result():
-    return render_template('poll/result.html')
+    return render_template('poll/result.html', user_id=get_user_id())
 
+@bp.route('/result/user', methods=('GET',))
+def user_data():
+    return overall_data(user_id=get_user_id())
+
+@bp.route('/result/overall', methods=('GET',))
+def overall_data(user_id = None):
+    db = get_db()
+    poll_query = db.execute('SELECT *, strftime(\'%s\', poll_time) as pt FROM poll WHERE expired != 2 ORDER BY id')
+
+    result = {}
+    for poll_row in poll_query:
+        options = poll_row['options'].split(',')
+        poll = {opt: 0 for opt in options}
+        poll.update({(opt + ' score'): 0 for opt in options})
+        poll['total_score'] = 0
+        poll['options'] = options[:]
+        options.insert(0, 'No answer')
+        result[poll_row['id']] = poll
+
+        if user_id:
+            user_query = db.execute(
+                'SELECT *, strftime(\'%s\', created) as ct  FROM bet '+
+                'WHERE user_id=? AND poll_id=? ORDER BY id', (user_id, poll_row['id'],))
+
+        else:
+            user_query = db.execute(
+                'SELECT *, strftime(\'%s\', created) as ct  FROM bet WHERE poll_id=? ORDER BY id', (poll_row['id'],))
+        for user_row in user_query:
+            choice = user_row['choice']
+            option_name = options[choice]
+            poll[option_name] += 1
+            if poll_row['correct_value'] == choice:
+                score = calculate_score(poll_row, user_row)
+                poll[option_name + ' score'] += score
+                poll['total_score'] += score
+
+
+    return jsonify(result)
+
+BASE_SCORE = 1000
+def calculate_score(poll, choice):
+    time_diff = int(choice['ct']) - int(poll['pt'])
+    offset = round(time_diff/30)
+    return BASE_SCORE // offset
--- static/js/loader.js
+++ static/js/loader.js
@@ -8,7 +8,7 @@ setInterval(function(){
       $.get(
         '/poll/current',
         function(data) {
-          if(id && data.id !== id) {
+          if(id && data.id !== id || data.expired) {
             form.submit();
           }
           if(!form.find('.question').length) {
@@ -16,7 +16,7 @@ setInterval(function(){
             for(var i = data.options.length - 1; i >= 0; i--) {
               form.prepend(
                 '<div class="form-group">'
-                +'<button name="option" class="question form-control" value="'+i+'" >' + data.options[i] + '</button></div>'
+                +'<button name="option" class="question form-control" value="'+(i + 1)+'" >' + data.options[i] + '</button></div>'
                 
               );
             }
